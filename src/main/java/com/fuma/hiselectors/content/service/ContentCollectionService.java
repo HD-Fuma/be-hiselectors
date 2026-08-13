3a4856813a5a9ba4b5e9dc8d1897ea65d6c65694
nunomi0 <ykys1@naver.com>
2026-08-13 22:13:29 +0900

    Chore: 신규 콘텐츠 수집 서비스 TODO 추가

---
+++ src/main/java/com/fuma/hiselectors/content/service/ContentCollectionService.java
@@ -0,0 +1,13 @@
+package com.fuma.hiselectors.content.service;
+
+import org.springframework.stereotype.Service;
+
+/** 신규 셀렉터스 콘텐츠 수집 흐름 */
+@Service
+public class ContentCollectionService {
+
+    // TODO: RawContent 중 최종 수집 시각 이후 콘텐츠 선별
+    // TODO: 셀렉터스 콘텐츠 판별
+    // TODO: Content·버전·미디어 저장 및 최종 수집 시각 갱신
+    // TODO: 콘텐츠 단건 반복 대신 묶음 처리 적용
+}
